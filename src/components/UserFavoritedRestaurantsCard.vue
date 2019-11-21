194d3bf0ed98d6c2c76834bc25bf22e6112a90b7
KerwinJhong <sh940718@gmail.com>
2019-11-22 01:47:32 +0800

    Create User Profile

---
+++ src/components/UserFavoritedRestaurantsCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <strong>{{favoritedRestaurants.length}}</strong> 收藏的餐廳
+    </div>
+    <div class="card-body">
+      <a
+        v-for="favoritedRestaurant in favoritedRestaurants"
+        :key="favoritedRestaurant.id"
+        href="/restaurants/3"
+      >
+        <img :src="favoritedRestaurant.image" width="60" height="60" class="avatar" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    favoritedRestaurants: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
---
+++ src/components/UserProfileCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="row">
+    <div class="col-md-12"></div>
+    <div class="col-md-4">
+      <img
+        class="img-responsive center-block"
+        src="{{profile.image}}"
+        style="width: 250px;margin-bottom: 25px;"
+      />
+    </div>
+    <div class="col-md-8">
+      <div class="well">
+        <ul class="list-unstyled">
+          <li>
+            <h5>{{profile.name}}</h5>
+          </li>
+          <li>
+            <p>{{profile.email}}</p>
+          </li>
+          <li>
+            <strong>{{restaurants.length}}</strong> 已評論餐廳
+          </li>
+          <li>
+            <strong>{{favoritedRestaurants.length}}</strong> 收藏的餐廳
+          </li>
+          <li>
+            <strong>{{followings.length}}</strong> Followings (追蹤者)
+          </li>
+          <li>
+            <strong>{{followers.length}}</strong> Followers (追隨者)
+          </li>
+          <li>
+            <a class="btn btn-primary mt-3" href="/users/{{profile.id}}/edit">edit</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    initialUser: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      user: this.initialUser
+    };
+  },
+  methods: {
+    addFollow() {
+      this.users = {
+        ...this.users,
+        isFollowed: true
+      };
+    },
+    deleteFollow() {
+      this.users = {
+        ...this.users,
+        isFollowed: false
+      };
+    }
+  }
+};
+</script>
