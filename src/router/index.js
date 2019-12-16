8f1f11e904b1069503b43a6bed6bb04be44ca1f5
KerwinJhong <sh940718@gmail.com>
2019-12-16 13:02:31 +0800

    vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,165 +1,145 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import NotFound from '../views/NotFound.vue'
-import SignIn from '../views/SignIn.vue'
-import Restaurants from '../views/Restaurants.vue'
-import store from '../store'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+// import store from '../store';
+import NotFound from '../views/NotFound.vue';
+import SignIn from '../views/SignIn.vue';
+import SignUp from '../views/SignUp.vue';
+import Restaurants from '../views/Restaurants.vue';
+const credit = JSON.parse(localStorage.getItem('credit'));
 
-Vue.use(Router)
+Vue.use(VueRouter);
 
-const authorizeIsAdmin = (to, from, next) => {
-  const currentUser = store.state.currentUser
-  if (currentUser && !currentUser.isAdmin) {
-    next('/404')
-    return
+const routes = [{
+    path: '/',
+    name: 'root',
+    redirect: '/restaurants'
+  },
+  {
+    path: '/admin',
+    exact: true,
+    redirect: '/admin/restaurants'
+  },
+  {
+    path: '/admin/restaurants',
+    name: 'admin-restaurants',
+    component: () =>
+      import ('../views/AdminRestaurants.vue')
+  },
+  {
+    path: '/admin/categories',
+    name: 'admin-categories',
+    component: () =>
+      import ('../views/AdminCategories.vue')
+  },
+  {
+    path: '/admin/users',
+    name: 'admin-users',
+    component: () =>
+      import ('../views/AdminUsers.vue')
+  },
+  {
+    path: '/admin/restaurants/create',
+    name: 'AdminRestaurantEdit',
+    component: () =>
+      import ('../views/AdminRestaurantEdit.vue')
+  },
+  {
+    path: '/admin/restaurants/:id',
+    name: 'admin-restaurant',
+    component: () =>
+      import ('../views/AdminRestaurant.vue')
+  },
+  {
+    path: '/admin/restaurants/:id/edit',
+    name: 'admin-restaurant-edit',
+    component: () =>
+      import ('../views/AdminRestaurantEdit.vue')
+  },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/restaurants',
+    name: 'Restaurants',
+    component: Restaurants
+  },
+  {
+    path: '/restaurants/top',
+    name: 'RestaurantsTop',
+    component: () =>
+      import ('../views/RestaurantsTop.vue')
+  },
+  {
+    path: '/restaurants/feeds',
+    name: 'RestaurantsFeeds',
+    component: () =>
+      import ('../views/RestaurantsFeeds.vue')
+  },
+  {
+    path: '/restaurants/:id',
+    name: 'restaurant',
+    component: () =>
+      import ('../views/Restaurant.vue')
+  },
+  {
+    path: '/restaurants/:id/dashboard',
+    name: 'RestaurantDashboard',
+    component: () =>
+      import ('../views/RestaurantDashboard.vue')
+  },
+  {
+    path: '/users/top',
+    name: 'UsersTop',
+    component: () =>
+      import ('../views/UsersTop.vue')
+  },
+  {
+    path: '/users/:id',
+    name: 'User',
+    component: () =>
+      import ('../views/User.vue')
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
   }
-  next()
-}
+];
 
-const router = new Router({
+const router = new VueRouter({
   linkExactActiveClass: 'active',
   mode: 'history',
-  routes: [{
-      path: '/',
-      name: 'root',
-      redirect: '/signin'
-    },
-    {
-      path: '/signin',
-      name: 'sign-in',
-      component: SignIn
-    },
-    {
-      path: '/signup',
-      name: 'sign-up',
-      component: () =>
-        import ('../views/SignUp.vue')
-    },
-    {
-      path: '/restaurants',
-      name: 'restaurants',
-      component: Restaurants
-    },
-    {
-      path: '/restaurants/feeds',
-      name: 'RestaurantsFeeds',
-      component: () =>
-        import ('../views/RestaurantsFeeds.vue')
-    },
-    {
-      path: '/restaurants/top',
-      name: 'RestaurantsTop',
-      component: () =>
-        import ('../views/RestaurantsTop.vue')
-    },
-    {
-      path: '/restaurants/:id',
-      name: 'restaurant',
-      component: () =>
-        import ('../views/Restaurant.vue')
-    },
-    {
-      path: '/restaurants/:id/dashboard',
-      name: 'RestaurantDashboard',
-      component: () =>
-        import ('../views/RestaurantDashboard.vue')
-    },
-    {
-      path: '/users/top',
-      name: 'users-top',
-      component: () =>
-        import ('../views/UsersTop.vue')
-    },
-    {
-      path: '/user/:id',
-      name: 'user',
-      component: () =>
-        import ('../views/User.vue')
-    },
-    {
-      path: '/user/:id/edit',
-      name: 'user-edit',
-      component: () =>
-        import ('../views/UserEdit.vue')
-    },
-    {
-      path: '/admin',
-      exact: true,
-      redirect: '/admin/restaurants'
-    },
-    {
-      path: '/admin/users',
-      name: 'admin-users',
-      component: () =>
-        import ('../views/AdminUsers.vue'),
-      beforeEnter: authorizeIsAdmin
-    },
-    {
-      path: '/admin/restaurants',
-      name: 'admin-restaurants',
-      component: () =>
-        import ('../views/AdminRestaurants.vue'),
-      beforeEnter: authorizeIsAdmin
-    },
-    {
-      path: '/admin/restaurants/new',
-      name: 'admin-restaurant-new',
-      component: () =>
-        import ('../views/AdminRestaurantNew.vue'),
-      beforeEnter: authorizeIsAdmin
-    },
-    {
-      path: '/admin/categories',
-      name: 'admin-categories',
-      component: () =>
-        import ('../views/AdminCategories.vue'),
-      beforeEnter: authorizeIsAdmin
-    },
-    {
-      path: '/admin/restaurants/:id',
-      name: 'admin-restaurant',
-      component: () =>
-        import ('../views/AdminRestaurant.vue'),
-      beforeEnter: authorizeIsAdmin
-    },
-    {
-      path: '/admin/restaurants/:id/edit',
-      name: 'admin-restaurant-edit',
-      component: () =>
-        import ('../views/AdminRestaurantEdit.vue'),
-      beforeEnter: authorizeIsAdmin
-    },
-    {
-      path: '*',
-      name: 'not-found',
-      component: NotFound
-    }
-  ]
-})
+  routes
+});
 
 router.beforeEach(async(to, from, next) => {
-  const tokenInLocalStorage = localStorage.getItem('token')
-  const tokenInStore = store.state.token
-  let isAuthenticated = store.state.isAuthenticated
-
-  // 比較 localStorage 和 store 中的 token 是否一樣
-  if (tokenInLocalStorage && tokenInLocalStorage !== tokenInStore) {
-    isAuthenticated = await store.dispatch('fetchCurrentUser')
+  if (!credit && to.name !== 'SignIn' && to.name !== 'SignUp') {
+    next('/signin');
+    return;
   }
 
-  // 如果 token 無效則轉址到登入頁
-  if (!isAuthenticated && to.name !== 'sign-in') {
-    next('/signin')
-    return
+  if (credit) {
+    if (to.name === 'SignIn' || to.name === 'Signup') {
+      next('/restaurants');
+      return;
+    }
   }
 
-  // 如果 token 有效則轉址到餐聽首頁
-  if (isAuthenticated && to.name === 'sign-in') {
-    next('/restaurants')
-    return
+  if (credit && credit.user.isAdmin === false) {
+    if (to.path.includes('/admin')) {
+      next('/404');
+      return;
+    }
   }
 
-  next()
+  next();
 })
 
-export default router
+export default router;
